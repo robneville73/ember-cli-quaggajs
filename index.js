3acf5adb8d6d6d248bda19d54b971f8359507eb6
Rob Neville <robertneville73@gmail.com>
2017-02-28 09:17:35 -0500

    exporting shim

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ module.exports = {
 
   included(app) {
     this._super.included.apply(this, arguments);
-    this.app.import(app.bowerDirectory + '/quagga/dist/quagga.min.js')
+    this.app.import(app.bowerDirectory + '/quagga/dist/quagga.min.js');
+    this.app.import('vendor/quagga.shim.js', {
+      type: 'vendor',
+      exports: { 'quagga': ['default'] }
+    });
   }
 };
