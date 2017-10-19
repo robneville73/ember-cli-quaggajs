be3b1f320e42bd282d59e20cd7dcb74c53c286a6
Rob Neville <robertneville73@gmail.com>
2017-10-19 12:56:29 -0400

    fix main index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ module.exports = {
 
   included(app) {
     this._super.included.apply(this, arguments);
-    this.app.import(app.bowerDirectory + '/quagga/dist/quagga.min.js');
-    this.app.import('vendor/quagga.shim.js', {
-      type: 'vendor',
-      exports: { 'quagga': ['default'] }
-    });
+    app.import('quagga');
   }
 };
